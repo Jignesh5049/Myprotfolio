9b247da8df2e4c85c7e0bbeb4fe80c694c0dc9ed
Jignesh5049 <prajapatijignesh225@gmail.com>
2025-11-27 19:19:58 +0530

    Update project

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const base =
+  process.env.VITE_DEPLOY_TARGET === 'gh-pages' ? '/protfolio/' : '/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base,
 })
